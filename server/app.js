2690f833dc530d59cef3128ab031131813cd00ba
ibrahimzaman <ibrahimzaman1129@gmail.com>
2023-08-26 13:57:10 +0530

    Aug 26 (1)

--- server/app.js
+++ server/app.js
@@ -30,44 +30,46 @@ app.post(`/savedata`, async (req, res) => {
             await fs.writeFile('data.json', JSON.stringify(fileData));
         }
 
-        // saveUserData();
+        saveUserData();
+
         res.status(200).json({
             success: true
         })
+        
     } catch (error) {
         console.error(error);
     }
 })
 
-app.post(`/pay`, async (req, res) => {
-    try {
-        async function pay() {
+// app.post(`/pay`, async (req, res) => {
+//     try {
+//         async function pay() {
 
-            let amount;
-            amount = req.body.amount
+//             let amount;
+//             amount = req.body.amount
 
-            var instance = new Razorpay({
-                key_id: `rzp_test_yZ0fUpgIh9QxTg`,
-                key_secret: `8wTwXbsqYgdE5R8MSUN0Jr35`
-            })
+//             var instance = new Razorpay({
+//                 key_id: `rzp_test_yZ0fUpgIh9QxTg`,
+//                 key_secret: `8wTwXbsqYgdE5R8MSUN0Jr35`
+//             })
 
-            let order = await instance.orders.create({
-                amount: amount * 100,
-                currency: "INR",
-                receipt: "receipt#1"
-            })
+//             let order = await instance.orders.create({
+//                 amount: amount * 100,
+//                 currency: "INR",
+//                 receipt: "receipt#1"
+//             })
 
-            res.status(201).json({
-                success: true,
-                order,
-                amount
-            })
-        }
-        pay();
-    } catch (error) {
-        console.error(error);
-    }
-})
+//             res.status(201).json({
+//                 success: true,
+//                 order,
+//                 amount
+//             })
+//         }
+//         pay();
+//     } catch (error) {
+//         console.error(error);
+//     }
+// })
 
 app.listen(port, () => {
     console.log(`Server is live at port:`,port);
